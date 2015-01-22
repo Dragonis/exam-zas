10b9f185aff148dda401cd98b104a8115a8b003b
WojtekSasiela <wojtek.sasiela@gmail.com>
2015-01-22 09:56:52 +0100

    generate Klient entity

---
+++ src/BDDTutorial/WojtekSasielaBundle/Entity/Klient.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace BDDTutorial\WojtekSasielaBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Klient
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Klient
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="imie", type="string", length=255)
+     */
+    private $imie;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nazwisko", type="string", length=255)
+     */
+    private $nazwisko;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set imie
+     *
+     * @param string $imie
+     * @return Klient
+     */
+    public function setImie($imie)
+    {
+        $this->imie = $imie;
+
+        return $this;
+    }
+
+    /**
+     * Get imie
+     *
+     * @return string 
+     */
+    public function getImie()
+    {
+        return $this->imie;
+    }
+
+    /**
+     * Set nazwisko
+     *
+     * @param string $nazwisko
+     * @return Klient
+     */
+    public function setNazwisko($nazwisko)
+    {
+        $this->nazwisko = $nazwisko;
+
+        return $this;
+    }
+
+    /**
+     * Get nazwisko
+     *
+     * @return string 
+     */
+    public function getNazwisko()
+    {
+        return $this->nazwisko;
+    }
+}
