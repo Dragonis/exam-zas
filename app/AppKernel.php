8fbdfa60a5d24073350492dd2738ebb75fbbe32c
WojtekSasiela <wojtek.sasiela@gmail.com>
2015-01-22 11:31:54 +0100

    NAPRAWA z 2 bundles w AppKernel.php

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,8 +21,7 @@ class AppKernel extends Kernel
             new My\UserBundle\MyUserBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new BDDTutorial\WojtekSasielaBundle\BDDTutorialWojtekSasielaBundle(),
-            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle()
-
+       
             );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
