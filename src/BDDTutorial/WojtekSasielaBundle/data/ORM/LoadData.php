c04203fd77610480a0da2161d7948a758e252f18
WojtekSasiela <wojtek.sasiela@gmail.com>
2015-01-22 10:33:05 +0100

    Fixtures: Dostawy & Zamowienia

--- src/BDDTutorial/WojtekSasielaBundle/data/ORM/LoadData.php
+++ src/BDDTutorial/WojtekSasielaBundle/data/ORM/LoadData.php
@@ -8,6 +8,8 @@ use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Yaml\Yaml;
 
 use Gajdaw\BDDTutorial\WojtekSasielaBundle\Entity\Towar;
+use Gajdaw\BDDTutorial\WojtekSasielaBundle\Entity\Dostawa;
+use Gajdaw\BDDTutorial\WojtekSasielaBundle\Entity\Zamowienia;
 
 class LoadData implements FixtureInterface
 {
@@ -19,11 +21,26 @@ class LoadData implements FixtureInterface
 
         $filename = __DIR__ . '/../../data/towary.yml';
         $yml = Yaml::parse(file_get_contents($filename));
+        // towary
         foreach ($yml as $item) {
             $towary = new Towar();
             $towary->setnazwa($item['Nazwa']);
             $towary->setCenaJednostkowa($item['CenaJednostkowa']);
             $manager->persist($towary);
+        }
+        //dostawy
+        foreach ($yml as $item) {
+            $dostawa = new Dostawa();
+            $dostawa->setnazwa($item['Nazwa']);
+            $dostawa->setCenaJednostkowa($item['CenaJednostkowa']);
+            $manager->persist($dostawa);
+        }
+        //zamowienia
+        foreach ($yml as $item) {
+            $zamowienia = new Zamowienia();
+            $zamowienia->setnazwa($item['Nazwa']);
+            $zamowienia->setCenaJednostkowa($item['CenaJednostkowa']);
+            $manager->persist($zamowienia);
         }
 
         $manager->flush();
